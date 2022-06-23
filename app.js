2b87547e675ffb2dd9bd82b39d165ea1936840cc
Nonso Johnpaul Akamnonu <92590162+CN-JP@users.noreply.github.com>
2022-06-23 17:28:11 +0100

    Additional features and fixes to number rating

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const btn = document.getElementById('calculate');
 
 btn.addEventListener('click', function() {
 
-  let probability = document.querySelector('#probability').value;
-  let impact = document.querySelector('#impact').value;
+  let probability = document.getElementById('probability').value;
+  let impact = document.getElementById('impact').value;
 
 if(probability === '' || impact === ''){
   alert("Please fill out the input field(s)");
@@ -15,21 +15,36 @@ let RiskRating = (probability * impact);
 
 RiskRating = RiskRating.toFixed(0);
 
-document.querySelector('#result').innerHTML = RiskRating;
+document.getElementById('result').innerHTML = RiskRating;
 
 let status = '';
 
-if (RiskRating < 3) {
-  status = "Low Risk";
-} else if (RiskRating >= 4 && RiskRating < 6) {
-  status = "Medium Risk";
-} else if (RiskRating >= 6 && RiskRating <= 9) {
+if (RiskRating <= 3) {
+  status = "Low";
+} else if (RiskRating > 3 && RiskRating <= 15) {
+  status = "Medium";
+} else if (RiskRating > 15 && RiskRating <= 25) {
   status = "High";
-} else if (RiskRating > 9) {
+} else {
   status = "Out of range. Enter a value according to the guideline.";
 }
 
 document.querySelector('.comment').innerHTML = `Result:
 Risk Rating is <span id ="comment">${status}</span>`;
+
+
+// recommendation
+let decision = '';
+
+if (status = "Low") {
+  decision = "Keep a tab on it!";
+} else if (status = "Medium") {
+  decision = "Watch closely and ensure adequate controls are present!";
+} else if (status = "High") {
+  decision = "Urgent measures to be taken to address the risk!";
+}
+
+document.querySelector('.recommendation-text').innerHTML = `Recommendation: ${decision}`;
+
 
 })
